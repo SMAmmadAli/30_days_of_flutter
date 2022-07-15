8ce8e730bef562b3afb8c90393e43261aa4ff706
SMAmmadAli <smammadali05@gmail.com>
2022-07-15 18:15:40 +0500

    First commit

---
+++ full_flutter/android/app/src/main/kotlin/com/example/full_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.full_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ full_flutter/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Material(
+        child: Center(
+          child: Container(
+            child: Text("Hello Flutter")
+          ),
+        ),
+      ),      
+    );
+  }
+}
