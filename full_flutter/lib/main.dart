18596a58cfcc1c622cf6b82a51043545a1abd343
SMAmmadAli <smammadali05@gmail.com>
2022-07-21 17:11:57 +0500

    Day 11 completed

--- full_flutter/lib/main.dart
+++ full_flutter/lib/main.dart
@@ -3,19 +3,38 @@ import 'package:full_flutter/Pages/Login_page.dart';
 import 'package:full_flutter/Pages/Home.dart';
 import 'package:full_flutter/utils/routes.dart';
 import 'package:full_flutter/widgets/themes.dart';
-
+import 'package:flutter/services.dart';
+import 'dart:convert';
 
 void main(List<String> args) {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final CatalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(CatalogJson);
+    var productData = decodedData("products");
+    print(productData);
+  }
+
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      
+
       // home: HomePage(),
       themeMode: ThemeMode.light,
       darkTheme: MyTheme.darktheme(context),
@@ -25,10 +44,9 @@ class MyApp extends StatelessWidget {
 
       routes: {
         "/": (context) => LoginPage(),
-        MyRoutes.homeRoute : (context) => HomePage(),
-        MyRoutes.loginRoute : (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
       },
     );
-
   }
 }
