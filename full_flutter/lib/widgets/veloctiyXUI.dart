f809d22a68aebf7b427e2d7a0cf42b326100591a
SMAmmadAli <smammadali05@gmail.com>
2022-07-28 20:07:15 +0500

    Day 14 Completed

--- full_flutter/lib/widgets/veloctiyXUI.dart
+++ full_flutter/lib/widgets/veloctiyXUI.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:full_flutter/models/catalogs.dart';
+import 'package:full_flutter/utils/routes.dart';
 
 class XUI extends StatelessWidget {
-  const XUI({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("VELOCITY X"),
-      
+    return  Card(
+      shadowColor: Colors.blue,
+      child: ListTile(
+        onTap: () => Navigator.pushNamed(context, MyRoutes.gridViewRoute),
+        leading: Image.network("https://pixabay.com/images/search/png/"),
+        title: Text("Yellow Color Car"),
+        subtitle: Text("Car"),
+        trailing: ElevatedButton(onPressed: () => Navigator.pushNamed(context, MyRoutes.loginRoute),
+        child: Text("Click me"),),
+      ),
     );
   }
 }
