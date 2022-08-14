e40e45be6bb443841e8eb0d5281e4483d0fd2753
SMAmmadAli <smammadali05@gmail.com>
2022-08-14 20:10:47 +0500

    Day 20 Completed

--- full_flutter/lib/Pages/cart_page.dart
+++ full_flutter/lib/Pages/cart_page.dart
@@ -18,17 +18,17 @@ class CartPage extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Column(
-              children: [
-                 Padding(
-                   padding: const EdgeInsets.all(16.0),
-                   child: Expanded(child: Placeholder()),
-                 ),
-                 Divider(),
-                 _cartTotal(),
-              ],
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: _CartList(),
             ),
+            Divider(),
+            _cartTotal(),
+          ],
         ),
-      );
+      ),
+    );
   }
 }
 
@@ -37,54 +37,62 @@ class _cartTotal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-        height: MediaQuery.of(context).size.height * 0.4,
+        // height: MediaQuery.of(context).size.height * 0.4,
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Text("\$349",
-                style: TextStyle(
-                  fontSize: 28,
-                  color: Theme.of(context).secondaryHeaderColor,
-                )),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.2,
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(
-                        Theme.of(context).primaryColor)),
-                child: Text("Buy",
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Theme.of(context).secondaryHeaderColor,),
-              ),
-            ))
-          ],
-        ));
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Text("\$349",
+            style: TextStyle(
+              fontSize: 28,
+              color: Theme.of(context).secondaryHeaderColor,
+            )),
+        SizedBox(
+            // width: MediaQuery.of(context).size.width * 0.2,
+            child: ElevatedButton(
+          onPressed: () {},
+          style: ButtonStyle(
+              backgroundColor:
+                  MaterialStateProperty.all(Theme.of(context).primaryColor)),
+          child: Text(
+            "Buy",
+            style: TextStyle(
+              fontSize: 18,
+              color: Theme.of(context).secondaryHeaderColor,
+            ),
+          ),
+        ))
+      ],
+    ));
   }
 }
 
-// class _CartList extends StatefulWidget {
-//   @override
-//   State<_CartList> createState() => __CartListState();
-// }
+class _CartList extends StatefulWidget {
+  @override
+  State<_CartList> createState() => __CartListState();
+}
 
-// class __CartListState extends State<_CartList> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return ListView.builder(
-//         itemCount: 5,
-//         itemBuilder: (context, index) => ListTile(
-//           leading: Icon(Icons.done, color: Theme.of(context).primaryColor,),
-//           trailing: IconButton(icon: Icon(Icons.delete, color: Theme.of(context).primaryColor),
-//           onPressed: (){},
-//           ),
-//           title: Text("Item 1",
-//                     style: TextStyle(
-//                     fontSize: 28,
-//                     color: Theme.of(context).secondaryHeaderColor,
-//                   )),
-//         ),
-//     );
-//   }
-// }
+class __CartListState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,
+      itemCount: 5,
+      itemBuilder: (context, index) => ListTile(
+        leading: Icon(
+          Icons.done,
+          color: Theme.of(context).primaryColor,
+        ),
+        trailing: IconButton(
+          icon: Icon(Icons.delete, color: Theme.of(context).primaryColor),
+          onPressed: () {},
+        ),
+        title: Text("Item 1",
+            style: TextStyle(
+              fontSize: 28,
+              color: Theme.of(context).secondaryHeaderColor,
+            )),
+      ),
+    );
+  }
+}
